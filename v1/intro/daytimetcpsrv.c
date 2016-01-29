27b5ec7e06af12358a99c2a687af4b81c11ec7ef
nicolerobin <lit050528@gmail.com>
2016-01-29 09:51:33 +0800

    add daytimetcpcli1 daytimetcpsrv1

---
+++ v1/intro/daytimetcpsrv.c
@@ -0,0 +1,36 @@
+/*
+ * 使用端口13，启动server时提示bind失败，权限不够。
+ * 使用root账户正常。
+ */
+#include	"unp.h"
+#include	<time.h>
+
+int
+main(int argc, char **argv)
+{
+	int					listenfd, connfd;
+	struct sockaddr_in	servaddr;
+	char				buff[MAXLINE];
+	time_t				ticks;
+
+	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family      = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port        = htons(13);	/* daytime server */
+
+	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+
+	Listen(listenfd, LISTENQ);
+
+	for ( ; ; ) {
+		connfd = Accept(listenfd, (SA *) NULL, NULL);
+
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+        Write(connfd, buff, strlen(buff));
+
+		Close(connfd);
+	}
+}
